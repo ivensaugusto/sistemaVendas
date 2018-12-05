75fa7ee04fd6df3658537dc6578c47c0b8c1b2d2
Ivens Augusto Ferreira Pinto <ivensaugusto@hotmail.com>
2018-12-05 12:32:01 -0200

    inserida pasta com script do banco de dados.

--- src/DAO/Database.java
+++ src/DAO/Database.java
@@ -1,15 +1,22 @@
 package DAO;
 
 public class Database {
-	String caminho = "jdbc:mysql://Localhost/";
-	String user = "root";
-	String senha = "";
+	String caminho;
+	String user;
+	String senha;
 	
 	public Database(String caminho, String user, String senha) {
 		super();
 		this.caminho = caminho;
 		this.user = user;
 		this.senha = senha;
+	}
+
+	public Database() {
+		super();
+		this.caminho = "jdbc:mysql://Localhost/db_sistemavendas";
+		this.user = "root";
+		this.senha = "";
 	}
 
 	public String getCaminho() {
